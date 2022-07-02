edb78b0fb380f2c282e31115ef71235cb68bc5d3
Wu Niutian <wuniutian@gmail.com>
2022-07-02 09:11:22 +0800

    Initial Commit

---
+++ Todo List Re-cap/Todo_List_Re_capApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_Re_capApp.swift
+//  Todo List Re-cap
+//
+//  Created by    wuniutian on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_Re_capApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
